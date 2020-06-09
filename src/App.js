9dca6a079bf14dd1535980577d06366e910a8058
Colin <colin.diesh@gmail.com>
2020-06-09 12:11:20 -0400

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const rootTree = {
+  name: "Mammal Ancestor",
+  children: [
+    {
+      name: "Primate ancestor",
+      children: [{ name: "Human" }, { name: "Ape" }],
+    },
+    { name: "Rodent ancestor", children: [{ name: "Mouse" }, { name: "Rat" }] },
+  ],
+};
+
+function Tree(props) {
+  const { tree, layer = 1 } = props;
+
+  const { name, children } = tree;
+  console.log(name, layer);
+
+  return (
+    <div>
+      <h1 style={{ fontSize: 150 - layer * 40 }}>{name}</h1>
+      {children
+        ? children.map((child) => <Tree tree={child} layer={layer + 1} />)
+        : null}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree tree={rootTree} />
     </div>
   );
 }
